6c384a2ed894874324121343a9076b85965481e6
mayuefan <m13160102112@163.com>
2020-01-20 01:28:52 +0800

    第一个版本

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # evan-switch
-uniapp switch开关组件
+uniapp开关组件，支持双向数据绑定，支持切换前事件拦截   
+测试过微信小程序、app（包括nvue模式）、h5
+
+### 1.基本用法
+
+```
+﻿<evan-switch v-model="checked"></evan-switch>
+```
+
+### 2.更多用法
+参考[github demo](https://github.com/EvanMaFYH/evan-switch)中的用法
+
+### evan-switch props
+| 参数           | 说明            | 类型    | 可选值     | 默认值  |    
+| :-------------------- | :------------------------------ | :---------- | :-------- | :--- |  
+| v-model | 开关选中状态 | string/number/boolean | - | false |
+| active-color | 打开状态的颜色 | string | - | ﻿#108ee9 |
+| inactive-color | 关闭状态的颜色 | string | - | #fff |
+| disabled | 是否禁用 | boolean | - | false |
+| size | 大小（单位px） | number | - | 30 |
+| active-value | 打开时的value值 | string/number/boolean | - | true |
+| inactive-value | 关闭时的value值 | string/number/boolean | - | false |
+| before-change | 开关状态切换前的钩子，如果返回false或者返回Promise且被reject，则状态不会被切换 | Function(e) | - | - |
+
+### evan-checkbox events
+| name | 说明 | 回调参数 |
+| :--- | :---------------- | ------------------|
+| change | 开关状态变更 | 变更后的状态 |
---
+++ pages/index/index.vue
@@ -0,0 +1,101 @@
+<template>
+	<view class="evan-switch-show">
+		<view class="evan-switch-show__title">
+			<text class="evan-switch-show__title__item">基础用法</text>
+		</view>
+		<evan-switch @change="onChagne" v-model="checked"></evan-switch>
+		<view class="evan-switch-show__title">
+			<text class="evan-switch-show__title__item">禁用</text>
+		</view>
+		<evan-switch :disabled="true" v-model="checked"></evan-switch>
+		<view class="evan-switch-show__title">
+			<text class="evan-switch-show__title__item">自定义颜色</text>
+		</view>
+		<evan-switch v-model="checked" activeColor="#00a231" inactiveColor="red"></evan-switch>
+		<view class="evan-switch-show__title">
+			<text class="evan-switch-show__title__item">自定义大小</text>
+		</view>
+		<evan-switch v-model="checked" :size="20"></evan-switch>
+		<view class="evan-switch-show__title">
+			<text class="evan-switch-show__title__item">自定义value值（当前选中的值：{{customChecked}}）</text>
+		</view>
+		<evan-switch v-model="customChecked" activeValue="value1" inactiveValue="value2"></evan-switch>
+		<view class="evan-switch-show__title">
+			<text class="evan-switch-show__title__item">拦截状态变更</text>
+		</view>
+		<evan-switch v-model="checked" :beforeChange="beforeChange"></evan-switch>
+		<view class="evan-switch-show__title">
+			<text class="evan-switch-show__title__item">拦截状态变更Promise</text>
+		</view>
+		<evan-switch v-model="checked" :beforeChange="beforeChangePromise"></evan-switch>
+
+		<!-- #ifdef APP-PLUS -->
+		<button @click="goNvue">nvue页面</button>
+		<!-- #endif -->
+	</view>
+</template>
+
+<script>
+	import EvanSwitch from '@/components/evan-switch/evan-switch.vue'
+	export default {
+		components: {
+			EvanSwitch
+		},
+		data() {
+			return {
+				checked: true,
+				customChecked: 'value1'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			beforeChange(e) {
+				uni.showToast({
+					title: '开关切换被拦截'
+				})
+				return false
+			},
+			beforeChangePromise(e) {
+				return new Promise((resolve, reject) => {
+					uni.showModal({
+						title: '提示',
+						content: `你确定要${e?'打开':'关闭'}吗`,
+						success: (res) => {
+							if (res.confirm) {
+								resolve()
+							} else if (res.cancel) {
+								reject()
+							}
+						}
+					})
+				})
+			},
+			onChagne(e){
+				console.log(e)
+			},
+			goNvue(nv) {
+				uni.navigateTo({
+					url: '/pages/app/app'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.evan-switch-show {
+		padding: 20px;
+
+		&__title {
+			padding: 10px 0;
+
+			&__item {
+				font-size: 16px;
+				color: #333;
+				font-weight: bold;
+			}
+		}
+	}
+</style>
